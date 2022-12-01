add1bf5c4c5e9923543d6257e8d9c95afa1111b0
Abewok <abewok@gmail.com>
2022-12-01 12:20:34 +0300

    new test

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,45 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
     from calculator_1 import add, sub, mul, div
-    count = len(argv)
-
-    if count != 4:
-        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+    from sys import argv
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-
-    num1 = int(argv[1])
-    num2 = int(argv[3])
-    op = argv[2]
-
-    def not_found():
+    if argv[2] in {"+", "-", "/", "*"}:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print("{} + {} = {}".format(a, b, add(a, b)))
+        if argv[2] == "-":
+            print("{} - {} = {}".format(a, b, sub(a, b)))
+        if argv[2] == "*":
+            print("{} * {} = {}".format(a, b, mul(a, b)))
+        if argv[2] == "/":
+            print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-
-    def my_add():
-        total = add(num1, num2)
-        print("{:d} + {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def my_sub():
-        total = sub(num1, num2)
-        print("{:d} - {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def my_mul():
-        total = mul(num1, num2)
-        print("{:d} * {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    def my_div():
-        total = div(num1, num2)
-        print("{:d} / {:d} = {:d}".format(num1, num2, total))
-        return total
-
-    options = {
-        "+": my_add,
-        "-": my_sub,
-        "*": my_mul,
-        "/": my_div
-    }
-    options.get(op, not_found)()
