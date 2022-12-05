507cb89dd092b475b08cb4e3e07c31b4807c30db
Abewok <abewok@gmail.com>
2022-12-05 09:10:18 +0300

    go

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,37 @@
-#include <stdio.h>
 #include "lists.h"
-/**
- * is_palindrome - is palindrome
- * @head: head node
- * Return: 1 if palindrome, 0 otherwise
- */
+
+listint_t *is_pal_recurs(listint_t *left, listint_t *right)
+{
+	if (right->next == NULL)
+		return (left->next);
+	right = right->next;
+	if (right->next == NULL)
+	{
+		if (left->n != left->next->n)
+			return (NULL);
+		if (left->next->next == NULL)
+			return (left);
+		return (left->next->next);
+	}
+	right = is_pal_recurs(left->next, right->next);
+	if (right == NULL)
+		return (NULL);
+	if (right->n == left->n)
+	{
+		if (right->next == NULL)
+			return (left);
+		return (right->next);
+	}
+	return (NULL);
+}
+		
+
 int is_palindrome(listint_t **head)
 {
-        listint_t *current = *head;
-        int tmp[100], i = 0, s = 0;
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
 
-        if (!*head || !head || !current->next)
-                return (1);
-
-        while (current)
-        {
-            tmp[i] = current->n;
-            i++;
-            current = current->next;
-        }
-        i--;
-        while (s <= i)
-        {
-            if(tmp[s] != tmp[i])
-                return (0);
-            s++;
-            i--;
-        }
-        return (1);
+	if (is_pal_recurs(*head, *head) != NULL)
+		return (1);
+	return (0);
 }
